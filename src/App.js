c5809d41d4687e2c70e97b162863728af287350d
rafa perez <rafael.perezruiz@gigas.com>
2022-08-30 11:10:17 +0200

    working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React, { useState, useRef } from "react";
 import Fieldset from "./Fieldset";
 var data = require("./output.json");
 
+function shuffle(array) {
+  var tmp, current, top = array.length;
+
+  if(top) while(--top) {
+      current = Math.floor(Math.random() * (top + 1));
+      tmp = array[current];
+      array[current] = array[top];
+      array[top] = tmp;
+  }
+
+  return array;
+}
+
+function getTests(questions, number) {
+  const shuffled = shuffle([...questions]);
+  return shuffled.slice(0, number);
+}
+
 function App() {
   const [questions, setQuestions] = useState([]);
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState(undefined);
   const formRef = useRef();
 
   const newTest = () => {
-    setQuestions(data.slice(0, 3));
+    setQuestions([]);
+    setTimeout(()=> setQuestions(getTests(data, 20)), 100);
+    // setTimeout(()=> setQuestions(data.slice(0, 3)), 100);
     formRef.current.reset();
     setResult("");
   };
@@ -34,7 +54,18 @@ function App() {
   });
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Mercancias peligrosas</h1>
+      <h1 className="text-3xl font-bold underline m-3 mb-10">Mercancias peligrosas</h1>
+      <form onSubmit={handleSubmit} ref={formRef} className="">
+        {tests}
+        <div className="flex justify-center m-10">
+          {questions.length > 0 && (
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Comprobar
+            </button>
+          )}
+        </div>
+      </form>
+      {result && <p className="flex justify-center font-bold text-3xl mb-20">Correctas: {result}</p>}
       <div className="flex justify-center m-10">
         <button
           onClick={newTest}
@@ -43,17 +74,6 @@ function App() {
           Nuevo Test
         </button>
       </div>
-      <form onSubmit={handleSubmit} ref={formRef} className="m-5">
-        {tests}
-        <div className="flex justify-center m-10">
-          {questions && (
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Comprobar
-            </button>
-          )}
-        </div>
-      </form>
-      {result && <p>Correctas: {result}</p>}
     </div>
   );
 }
--- src/Fieldset.js
+++ src/Fieldset.js
@@ -1,8 +1,16 @@
-function Radio({identifier, value, text}) {
+function Radio({identifier, value, text, answer, userAnswer}) {
+  let showBorder = undefined;
+  if (userAnswer && answer === value) {
+    if (userAnswer === answer) {
+      showBorder = "border border-green-600 bg-green-300 font-bold"
+    } else {
+      showBorder = "border border-red-600 bg-red-400 font-bold"
+    }
+  }
   return (
-    <div>
+    <div className={`mb-2 ${showBorder}`}>
       <input type="radio" id={`${identifier}${value}`} name={identifier} value={value} />
-      <label htmlFor={`${identifier}${value}`}>{text}</label>
+      <label htmlFor={`${identifier}${value}`} className="ml-2">{text}</label>
     </div>
   );
 }
@@ -12,15 +20,14 @@ function Fieldset({ data, showAnswer = false }) {
   const background = `${userAnswer}` === `${answer}` ? "green" : "red";
   return (
     <fieldset id={identifier} className="m-4 leading-loose">
-      <legend className="font-bold">{question}</legend>
-      <Radio identifier={identifier} value={"1"} text={data["1"]}/>
-      <Radio identifier={identifier} value={"2"} text={data["2"]}/>
-      <Radio identifier={identifier} value={"3"} text={data["3"]}/>
-      <Radio identifier={identifier} value={"4"} text={data["4"]}/>
+      <legend className="font-bold mb-5">{question}</legend>
+      <Radio identifier={identifier} value={"1"} text={data["1"]} answer={answer} userAnswer={userAnswer}/>
+      <Radio identifier={identifier} value={"2"} text={data["2"]} answer={answer} userAnswer={userAnswer}/>
+      <Radio identifier={identifier} value={"3"} text={data["3"]} answer={answer} userAnswer={userAnswer}/>
+      <Radio identifier={identifier} value={"4"} text={data["4"]} answer={answer} userAnswer={userAnswer}/>
       {userAnswer && (
         <div>
-          <p style={{background}} className="mt-4">{data[answer]}</p> 
-          <p>{norma}</p>
+          <p className="m-5 font-bold">{norma}</p>
         </div>
       )}
     </fieldset>
